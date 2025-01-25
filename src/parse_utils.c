a5833f552db65a088e0733d6e5bb7e474a810eba
RM <devrmoreira@gmail.com>
2025-01-25 16:11:40 +0000

    corrected the check_size function

--- src/parse_utils.c
+++ src/parse_utils.c
@@ -6,7 +6,7 @@
 /*   By: rimagalh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 15:31:03 by rimagalh          #+#    #+#             */
-/*   Updated: 2025/01/22 16:40:13 by rimagalh         ###   ########.fr       */
+/*   Updated: 2025/01/25 16:09:19 by rimagalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,28 @@ static int check_size(char *str)
 {
 	char* max = "2147483647";
 	char* min = "-2147483648";
+	size_t	len = ft_strlen(str);
 
-	if(ft_strncmp(str, max, ft_strlen(str)) >= 0)
-		return 1;
-	if(ft_strncmp(str, min, ft_strlen(str)) >= 0)
-		return 1;
-	return 0;
+	if(len > 11)
+		return (1);
+
+	if(str[0] == '-')
+	{
+		if(len == 11 && ft_strncmp(str, min, 11) > 0)
+			return (1);
+	}
+	else
+	{
+		if(str[0] == '+')
+			str++;
+
+		len = ft_strlen(str);
+		if(len == 10 && ft_strncmp(str, max, 10) > 0)
+			return (1);
+
+	}
+
+	return (0);
 }
 int validate_stack(int *arr, int size)
 {
